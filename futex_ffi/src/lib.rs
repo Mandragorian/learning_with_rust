aae01b08f2ac521096ba5335fd81d9c70036d94c
Konstantinos Andrikopoulos <mandragore@protonmail.com>
2021-10-13 01:32:34 +0200

    check that threads wake when waiting

--- futex_ffi/src/lib.rs
+++ futex_ffi/src/lib.rs
@@ -34,4 +34,23 @@ mod tests {
         let res = unsafe { futex(shared_int_addr_u64, FUTEX_WAKE, 1, 0) };
         assert_eq!(res, 0);
     }
+
+    #[test]
+    fn futex_wake_after_wait() {
+        let shared_int: u32 = 0;
+        let shared_int_addr = &shared_int as *const u32;
+        let shared_int_addr_u64 = shared_int_addr as u64;
+
+        let handle = std::thread::spawn(move || {
+            unsafe { futex(shared_int_addr_u64, FUTEX_WAIT, 0, 0) }
+        });
+
+        std::thread::sleep(std::time::Duration::from_millis(2000));
+        let res = unsafe { futex(shared_int_addr_u64, FUTEX_WAKE, 1, 0) };
+        assert_eq!(res, 1);
+
+        // Checking that the return value is zero checks both that 
+        // the thread was woken up, and with no errors
+        assert_eq!(handle.join().unwrap(), 0);
+    }
 }
