c261dd3b603a39cd6d4e653ecebe74de1ec7ee67
Konstantinos Andrikopoulos <mandragore@protonmail.com>
2021-10-14 01:19:18 +0200

    make Futer generic

--- futer/src/lib.rs
+++ futer/src/lib.rs
@@ -1,8 +1,10 @@
-pub struct Futer;
+pub struct Futer<T> {
+    _phantom: std::marker::PhantomData<T>,
+}
 
-impl Futer {
-    pub fn new(val: u32) -> Self {
-        Self
+impl<T> Futer<T> {
+    pub fn new(val: T) -> Self {
+        Self { _phantom: std::marker::PhantomData }
     }
 }
 
@@ -13,5 +15,11 @@ mod tests {
     #[test]
     fn futer_can_be_correctly_constructed() {
         let futer = Futer::new(32);
+    }
+
+    #[test]
+    fn futer_is_generic() {
+        let _futer_int: Futer<u32> = Futer::new(32);
+        let _futer_string: Futer<String> = Futer::new(String::from("hello"));
     }
 }
